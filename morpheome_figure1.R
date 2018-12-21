33f13344677c3fa36f1a354796048d45ce1b0117
tim-peterson <petersontimr@gmail.com>
2018-12-21 14:51:37 -0600

    adding to fig1a

--- morpheome_figure1.R
+++ morpheome_figure1.R
@@ -1,1 +1,6 @@
-p2 <- ggplot(cancer_vs_other, aes(x = `gene`, y = `citations`)) + geom_point(color = "#153049", size=1) + ylim(0,40000) + xlim(0,40000) + scale_x_continuous(trans='log10') + scale_y_continuous(trans='log10') + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = '#e8e7ea', colour = '#a7a9ac', size=1)) 
+gene_counts <- read_csv("~/OneDrive - Washington University in St. Louis/Data/MORPHEOME/Tim-MySQL-DB/gene-counts.csv", 
++     col_names = FALSE)
+
+library(ggplot2)
+
+fig1A <- ggplot(gene_counts, aes(x = as.numeric(row.names(gene_counts)), y = `X2`)) + geom_point(color = "#153049", size=1) + ylim(0,40000) + xlim(0,40000) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = '#e8e7ea', colour = '#a7a9ac', size=1)) + scale_x_discrete()
