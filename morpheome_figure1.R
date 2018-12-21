b1add62224c92e98d90168b213b20ca1b8c2b78d
tim-peterson <petersontimr@gmail.com>
2018-12-21 11:19:58 -0600

    first commit

---
+++ morpheome_figure1.R
@@ -0,0 +1,1 @@
+p2 <- ggplot(cancer_vs_other, aes(x = `gene`, y = `citations`)) + geom_point(color = "#153049", size=1) + ylim(0,40000) + xlim(0,40000) + scale_x_continuous(trans='log10') + scale_y_continuous(trans='log10') + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = '#e8e7ea', colour = '#a7a9ac', size=1)) 
